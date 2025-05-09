05a487e13bf86da49cd6894fe5b79234dd693ca0
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2025-05-09 22:04:57 +0300

    начало положено

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
+import {Films} from "./component/Films.jsx";
+import {useState} from "react";
 
 function App() {
+
+    const [films, setFilms] = useState([])
+
+    const resultFilms = (newFilm) => {
+        setFilms((prevFilms) => [...prevFilms, newFilm]);
+    }
 
 
   return (
     <>
       <h1>🧾 Список фильмов</h1>
+        <Films  resultFilms={resultFilms}/>
 
+        {films.map((f) => {
+            return (
+                <div>
+                    <ul>
+                        <li key={f.id}>
+                            {f.text} - {f.genre} - {f.question}
+                        </li>
+                    </ul>
+                </div>
+            )
+        })}
     </>
   )
 }
